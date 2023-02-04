6c0ffab955b332fa54a5e1a49655079a6dc1bd6f
Corches Ciprian <cipriancorches@gmail.com>
2023-02-04 16:22:20 +0200

    finalizarea problemei 8 ( C1)

--- functii.h
+++ functii.h
@@ -23,21 +23,37 @@ void afisare(int v[], int dim) {
 
 }
 void secventaMaxPara(int v[], int dim) {
-	int smax = 1;
-	int dmax = 0;
+	int stg = 1;
+	int dr = 0;
+	int stmp= 0;
+	int suma = 0;
+	int stgF = 0;
+	int drF = 0;
 	for (int i = 0; i < dim; i++) {
 		if (v[i] % 2 == 0) {
 			int j = i;
 			while (j + 1 < dim && v[j + 1] % 2 == 0) {
 				j++;
 			}
-			if (j - i + 1 > dmax - smax + 1) {
-				smax = i, dmax = j;
-
+			if (j - i + 1 > dr - stg + 1) {
+				suma = 0;
+				stg = i;
+				dr = j;
+				for (int f = stg; f < dr; f++) {
+					suma += v[f];
+				}
 			}
-
+			if (j - i + 1 == dr - stg + 1)  {
+				cout << suma << endl;
+				cout << stmp << endl;
+				if (suma > stmp) {
+					stgF = i;
+					drF = j;
+					
+				}
+			}
+			stmp = suma;
 		}
 	}
-	cout << smax << " " << dmax << endl;
-
+	cout << stgF << " " << drF << endl;
 }
