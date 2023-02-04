ec8ce5e33daf472326c728abcd663d1edea99ce1
Corches Ciprian <cipriancorches@gmail.com>
2023-02-04 15:49:31 +0200

    adaugat solutia problemei  8

--- functii.h
+++ functii.h
@@ -22,3 +22,22 @@ void afisare(int v[], int dim) {
 	cout << endl;
 
 }
+void secventaMaxPara(int v[], int dim) {
+	int smax = 1;
+	int dmax = 0;
+	for (int i = 0; i < dim; i++) {
+		if (v[i] % 2 == 0) {
+			int j = i;
+			while (j + 1 < dim && v[j + 1] % 2 == 0) {
+				j++;
+			}
+			if (j - i + 1 > dmax - smax + 1) {
+				smax = i, dmax = j;
+
+			}
+
+		}
+	}
+	cout << smax << " " << dmax << endl;
+
+}
--- solutii.h
+++ solutii.h
@@ -1,3 +1,10 @@
-#pragma once
+﻿#pragma once
 #include "functii.h"
-using namespace s
+using namespace std;
+
+void c1() {
+	//Determinați cea mai lungă secvență de elemente pare din vector. Dacă există mai multe secvențe cu lungime maximă se va determina cea cu suma elementelor maximă. Dacă există mai multe astfel de secvențe se va determina cea mai din stânga.
+	int v[100], d = 0;
+	citire(v, d);
+	secventaMaxPara(v, d); //???
+}
